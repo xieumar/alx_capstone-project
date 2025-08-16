cc7d58dc387697a754fca3e099dca39ad29083a5
Zainab Umar Ibrahim <xieumar@gmail.com>
2025-08-16 16:12:39 +0100

    completed hero section

---
+++ Trip-planner/src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import heroImage from "../assets/Hero-img.jpg";
+
+function Hero({ user = { name: "Uriel" } }) {
+  return (
+    <div className="hero h-[540px] w-full bg-center bg-[length:100%] bg-no-repeat flex justify-center items-center"
+    style={{ backgroundImage: `url(${heroImage})` }}
+    >
+      <div className="text-container">
+        <div className="hero-text text-stone-100 text-center">
+          <h1 className=" text-4xl">Good afternoon {user.name}</h1>
+          <div className="hero-btn w-[450px] h-[40px] bg-white rounded-lg flex items-center mt-4">
+            <input 
+            type="text px-12"
+            className="w-full h-full placeholder-black px-3 border-none rounded-l-lg"
+            placeholder="Location, place ....." />
+            <button className="bg-blue-800 items-center text-white rounded-r-lg h-[40px] px-4">
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- Trip-planner/src/components/Navbar.jsx
+++ Trip-planner/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { FaRegUser } from "react-icons/fa";
 
 function Navbar() {
   return (
-    <div className=' fixed top-0 left-0 right-0 bg-white shadow-md z-10 h-18 flex items-center justify-between px-8'>
+    <div className=' fixed top-0 left-0 right-0 bg-white/1 backdrop-blur-sm  z-10 h-18 flex items-center justify-between px-8'>
       <div className="icon flex w-[20%] items-center gap-2">
         <img src={locationIcon} alt="icon" />
         <p className='text-2xl'>Trip Planner</p>
@@ -15,24 +15,24 @@ function Navbar() {
         <input 
           type="text" 
           placeholder='Search for a destination' 
-          className='w-full h-10 px-8 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-800'
+          className='w-full h-10 px-8 border placeholder-black border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-900'
         />
-        <div className="search-icon absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-500">
+        <div className="search-icon absolute left-2 top-1/2 transform -translate-y-1/2 text-black-500">
             <GoSearch />
         </div>
       </div>
 
-    <div className="nav-links w-[35%] ml-30 flex items-center gap-8 text-gray-700">
-  {["Home", "Travel Guides", "Explore", "Trip Plans"].map((link) => (
-    <p
-      key={link}
-      className="relative cursor-pointer group transition-colors duration-300 hover:text-blue-800"
-    >
-      {link}
-      <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-blue-800 transition-all duration-300 group-hover:w-full"></span>
-    </p>
-  ))}
-</div>
+        <div className="nav-links w-[35%] ml-30 flex items-center gap-8 text-black-300 ">
+            {["Home", "Travel Guides", "Explore", "Trip Plans"].map((link) => (
+                <p
+                key={link}
+                className="relative cursor-pointer group transition-colors duration-300 hover:text-blue-800"
+                >
+                {link}
+                <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-blue-800 transition-all duration-300 group-hover:w-full"></span>
+                </p>
+            ))}
+        </div>
 
 
       <div className="user w-[5%] flex justify-center items-centers">
